dfed08857d80aacb7e7b76a34c78e1ba3c5ccf46
Kenny Yang <kennyyangh@gmail.com>
2020-09-23 22:42:46 -0700

    Updates

---
+++ precache-manifest.437fee235b9abff56309237cd24fbb9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1d85f09546ac9e14951c39b7d1081d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "5903e82ae96f98b2a8b3",
+    "url": "/react-gh-pages/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5903e82ae96f98b2a8b3",
+    "url": "/react-gh-pages/static/js/2.1a57ae6e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-gh-pages/static/js/2.1a57ae6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799d4139797bc707d724",
+    "url": "/react-gh-pages/static/js/main.ffaae46e.chunk.js"
+  },
+  {
+    "revision": "7f3037d2f7ce7e49aef5",
+    "url": "/react-gh-pages/static/js/runtime-main.71409b5e.js"
+  },
+  {
+    "revision": "ed8c3075808b9a2732b021093af9ac69",
+    "url": "/react-gh-pages/static/media/OfficialLogo.ed8c3075.jpg"
+  },
+  {
+    "revision": "123f496951bb0749d34cc0bd7f95da2e",
+    "url": "/react-gh-pages/static/media/mainPhoto.123f4969.jpg"
+  },
+  {
+    "revision": "9a1810dc2d0f35ea3904766bbe72cfbd",
+    "url": "/react-gh-pages/static/media/pictureofme.9a1810dc.jpg"
+  }
+]);
